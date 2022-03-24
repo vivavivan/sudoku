76b6bab9712b8d8bbef85e1f2d0d7182ea1ff557
vivavivan <vivan.chandrasekhara@gmail.com>
2022-03-24 20:55:55 +0100

    zahlenkorrektur

--- components/sudoku.js
+++ components/sudoku.js
@@ -8,33 +8,39 @@ app.component('sudoku', {
     template:
     /*html*/
     `
+    Possible Values: {{ possible }}<br/>
     <div id="sudoku" align="center">
     <table id="sudoku_grid" cellspacing="0" cellpadding="0">
         <tbody>
             <tr v-for="(row, rowindex) in board" 
             :id="'row' + rowindex" 
             :class="'sudoku_row sr' + rowindex%3">
-            <td v-for="(cell, cellindex) in row" 
-            :id="'cell' + rowindex + cellindex" 
-            :class="'sudoku_column sc' + cellindex%3" 
-            @mouseenter="mouseEnter($event, rowindex, cellindex)" 
-            @mouseleave="mouseLeave($event, rowindex, cellindex)">
+            <td v-for="(cell, columnindex) in row" 
+            :id="'cell' + rowindex + columnindex" 
+            :class="'sudoku_column sc' + columnindex%3" 
+            @mouseenter="mouseEnter($event, rowindex, columnindex)" 
+            @mouseleave="mouseLeave($event, rowindex, columnindex)">
                 <input class="sudoku_input" 
                 size="1" autocomplete="off" 
                 :readonly="cell!=0" 
-                :id="'input' + rowindex + cellindex" 
-                :value="cell==0?'':cell" 
-                v-on:click="rowncolumn">
+                :id="'input' + rowindex + columnindex" 
+                :value="cell==0?'':board[rowindex][columnindex]">
             </td>
             </tr>
         </tbody>
     </table>
     </div>
     `,
+    data() {
+        return {
+            possible: '',
+        }
+    },
     methods: {
         mouseEnter: function(element, row, column){
             this.crosshair(row, column, 'lightgray');
             this.box(row, column, 'lightgray');
+            this.possibilities(row, column);
             element.toElement.style.backgroundColor = 'gray';
         },
         mouseLeave: function(element, row, column){
@@ -51,10 +57,6 @@ app.component('sudoku', {
             }
 
         },
-        rowncolumn() {
-            console.log(elementBoard);
-
-        },
         box(row, column, color) {
             const startrow = Math.floor(row/3) * 3;
             const startcolumn = Math.floor(column/3) * 3;
@@ -64,6 +66,31 @@ app.component('sudoku', {
                     document.getElementById('cell' + rowindex + columnindex).style.backgroundColor = color;    
                 }   
             }    
+        },
+        possibilities(row, column) {
+            let p = new Set([1,2,3,4,5,6,7,8,9]);
+
+            for (let columnindex = 0; columnindex < 9; columnindex++) {
+                p.delete(this.board[row][columnindex]);
+            }
+
+            for (let rowindex = 0; rowindex < 9; rowindex++) {
+                p.delete(this.board[rowindex][column]);
+            }
+
+            const startrow = Math.floor(row/3) * 3;
+            const startcolumn = Math.floor(column/3) * 3;
+
+            for (let rowindex = startrow; rowindex < startrow + 3; rowindex++) {
+                for (let columnindex = startcolumn; columnindex < startcolumn + 3; columnindex++) {
+                    p.delete(this.board[rowindex][columnindex]);     
+                }   
+            }    
+
+            this.possible = '';
+            p.forEach(nr => {
+            this.possible += nr + ' ';
+            });
         }
     },
     computed: {
